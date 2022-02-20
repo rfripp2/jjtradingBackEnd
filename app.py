207ade69ab4988859398b16bab084346b3064408
juan gonzalez venzano <juangonzalez465@gmail.com>
2022-02-20 10:10:35 -0300

    das

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/api', methods=['GET'])
+@cross_origin()
 def index():
     return{
         "tutorial": "Flask React Heroku"
