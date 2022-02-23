e6b583114950944d5b486be5b4ef041bf09f0da4
juan gonzalez venzano <juangonzalez465@gmail.com>
2022-02-23 08:48:42 -0300

    adding exluded coins routes get and put

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from utils import *
 
+
 app = Flask(__name__)
 CORS(app)
 
@@ -20,12 +21,27 @@ def api():
     }
 
 
-@app.route('/prices', methods=['GET'])
+@app.route('/minsmax', methods=['GET'])
 @cross_origin()
 def prices():
     days_back = request.args.get('days_back')
     coins_quantity = request.args.get('coins_quantity')
     return get_coins_in_min_max(days_back, coins_quantity)
+
+
+@app.route('/minsmax/coinsexcluded', methods=['GET'])
+@cross_origin()
+def coins_excluded():
+    return json.dumps(coins_exceptions)
+
+
+@app.route('/minsmax/addcoin/', methods=['PUT'])
+@cross_origin()
+def add_coin():
+    coin = request.args.get('coin')
+    print(coin)
+    coins_exceptions.append(coin)
+    return json.dumps(coins_exceptions)
 
 
 if __name__ == '__main__':
