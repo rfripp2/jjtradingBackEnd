af888513dd8c2a7d88a74fadaffbf41d22625017
juan gonzalez venzano <juangonzalez465@gmail.com>
2022-02-26 20:30:48 -0300

    cors

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={
-            r"/api/*": {"origins": "*"}}, support_credentials=True)
+cors = CORS(app)
 
 
 @app.route('/')
