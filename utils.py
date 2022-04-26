e163435a12ca6dc9ecb67afdf142bb6f92213f0d
rfripp2 <juangonzalez465@gmail.com>
2022-04-25 22:43:18 -0300

    fixing binance api

--- utils.py
+++ utils.py
@@ -46,7 +46,7 @@ coins_exceptions = [
 # Request al endpoint de coingecko,por pagina max 250, osea que tengo que ajustar la funcion para que si se piden mas de 250, haga otro request a la page=2
 # Este endpoint retorna mucha inf de cada moneda,symbol,mc etc
 def get_coins_inf_cg(quantity):
-    
+
     coins = requests.get(
         f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page={quantity}&page=1&sparkline=false",
         timeout=6,
@@ -110,6 +110,8 @@ def is_today_min_high(pair, period):
     coin = pair.replace("-usd", "")
 
     df = ticker.history(period=period)
+    print(len(df), period)
+    print(df)
     result = {"min": False, "max": False, "error": False}
     # Inicializa min y max como la primera vela
 
@@ -130,23 +132,10 @@ def is_today_min_high(pair, period):
         elif max == df["Close"].iloc[-1]:
             result["max"] = True
     else:
-        result["error"] = True
         return mins_max_binance(coin, int(days))
 
     # La funcion retorna un objeto con Min,Max,Error que van a tener valores booleanso (True o False), en el front cuando hago este request a x moneda, si me retorna true,la muestra en la lista de min,max o error
     return result
-
-
-# Estas funciones las importo en otro archivo que es nuestra API (con la que nos comunicamos desde el front),por ej en la web,hacemos un request a jjtradingapi/api/minsMax, nuestra api en ese endpoint retorna la funcion is_today_min_max(), asi como goingecko retorna X cuando hacemos el request a su endpoint.
-# client_dydx = Client(host='https://api.dydx.exchange')
-
-
-def get_funding_rate():
-    url = "https://open-api.coinglass.com/api/pro/v1/futures/funding_rates_chart?symbol=BTC&type=U"
-    headers = {"coinglassSecret": "dc9b936244504e75a9582dcc7c728908"}
-    response = requests.request("GET", url, headers=headers)
-    response = response.json()
-    return response
 
 
 binance_key = os.environ.get("BINANCE_API_KEY")
@@ -156,18 +145,6 @@ binance_secret_key = os.environ.get("BINANCE_SECRET_KEY")
 # client_binance = Client(binance_key, binance_secret_key)
 
 # client_futures = Futures(binance_key, binance_secret_key)
-
-
-def funding_rate_binance():
-    futures_client = Client_Futures()
-    return futures_client.funding_rate("BTCUSDT", **{"limit": 1000})
-
-
-def historical_price_binance(interval):
-    spot_client = Client(binance_key, binance_secret_key)
-    data = spot_client.get_klines(symbol="BTCUSDT", interval=interval, limit=1000)
-
-    return data
 
 
 url = "https://rest.coinapi.io/v1/exchangerate/BTC/USD/history?period_id=1MIN&time_start=2016-01-01T00:00:00&time_end=2016-02-01T00:00:00"
@@ -183,38 +160,13 @@ def get_initial_date_from_days_back(daysback):
     return days_back_date
 
 
-def coin_api(coin, daysback):
-    days_back = get_initial_date_from_days_back(daysback)
-    now = datetime.now().replace(microsecond=00).isoformat()
-    coin = coin.upper()
-
-    url = f"https://rest.coinapi.io/v1/exchangerate/{coin}/USD/history?period_id=1DAY&time_start={days_back}&time_end={now}"
-    headers = {"X-CoinAPI-Key": "72785BE9-0392-4BDA-B115-596C3836840F"}
-    response = requests.get(url, headers=headers)
-    data = response.json()
-
-    result = {"min": False, "max": False, "error": False}
-    min = data[0]["rate_close"]
-    max = data[0]["rate_close"]
-    for candle in data:
-        if candle["rate_close"] < min:
-            min = candle["rate_close"]
-        elif candle["rate_close"] > max:
-            max = candle["rate_close"]
-
-    if min == data[-1]["rate_close"] == min:
-        result["min"] = True
-    elif max == data[-1]["rate_close"] == max:
-        result["max"] = True
-
-    return result
-
-
 def mins_max_binance(coin, daysback):
+    print("binance api")
 
     client = Spot()
     msnow = int(round(time.time() * 1000))
     symbol = coin.upper() + "USDT"
+    print("symbol:", symbol)
 
     start_date = datetime.today().replace(
         hour=21, minute=00, second=00, microsecond=00
@@ -227,8 +179,10 @@ def mins_max_binance(coin, daysback):
     )
 
     result = {"min": False, "max": False, "error": False}
+
     min = response[0][4]
     max = response[0][4]
+
     for candle in response:
         if candle[4] < min:
             min = candle[4]
@@ -238,5 +192,10 @@ def mins_max_binance(coin, daysback):
         result["min"] = True
     elif response[-1][4] == max:
         result["max"] = True
-
+    print("RESPONSE", coin, response)
+    print(result)
     return result
+
+
+# mins_max_binance("BTC", 7)
+# is_today_min_high("BTC-usd", "2d")
